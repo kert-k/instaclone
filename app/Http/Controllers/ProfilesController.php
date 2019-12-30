f846299a08562bfc5d1bcf4664e08f29ea317884
NeoHack0101 <31630039+NeoHack0101@users.noreply.github.com>
2019-12-30 15:19:46 +0200

    Add policies

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -14,11 +14,15 @@ class ProfilesController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user->profile);
+
         return view('profiles.edit', ['user' => $user]);
     }
 
     public function update(User $user)
     {
+        $this->authorize('update', $user->profile);
+
         $data = request()->validate([
             'title' => 'required',
             'description' => 'required',
@@ -26,7 +30,15 @@ class ProfilesController extends Controller
             'image' => ''
         ]);
         
-        \App\Profile::where('user_id', auth()->user()->id)->update($data);
+        if(request('image')){
+            $imagePath = request('image')->store('profileImages', 'public');
+    
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000,1000);
+            $image->save();
+
+        }
+
+        \App\Profile::where('user_id', auth()->user()->id)->update(array_merge($data, ['image' => $imagePath]));
 
         return redirect('/profile/'.auth()->user()->id);
     }
