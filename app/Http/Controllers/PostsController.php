d934957e3119dfe02500243be7056798ca3091a6
NeoHack0101 <31630039+NeoHack0101@users.noreply.github.com>
2020-01-12 18:01:46 +0200

    No humans allowed on images. (Clarifai API)

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 Use Intervention\Image\Facades\Image;
 use App\Post;
 
+use Clarifai\API\ClarifaiClient;
+use Clarifai\DTOs\Inputs\ClarifaiFileImage;
+use Clarifai\DTOs\Outputs\ClarifaiOutput;
+use Clarifai\DTOs\Predictions\Concept;
+
 class PostsController extends Controller
 {
     public function __construct()
@@ -31,10 +36,18 @@ class PostsController extends Controller
     }
 
     public function store()
-    {
+    {   
         $data = request()->validate([
             'caption' => 'required',
-            'image' => ['required', 'image']
+            'image' => [
+                'required',
+                'image',
+                function ($attribute, $value, $fail) {
+                    if ($this->detectHumanFace(request('image'))) {
+                        $fail('Image must not contain a human.');
+                    }
+                }
+                ]
         ]);
         
         $imagePath = request('image')->store('uploads', 'public');
@@ -48,17 +61,40 @@ class PostsController extends Controller
         $post->user_id = auth()->user()->id;
         $post->save();
 
-        //NOT WORKING
-        // auth()->user()->posts()->create([
-        //     'caption' => $data['caption'],
-        //     'image' => '/storage/' . $imagePath,
-        // ]);
-
         return redirect('/profile/' . auth()->user()->id);
     }
 
     public function show(\App\Post $post)
     {
         return view('posts.show', ['post' => $post]);
+    }
+
+    public function detectHumanFace($image)
+    {
+        $humanOnImage = false;
+
+        $client = new ClarifaiClient('b952af584e1b41e2b6a7a498fe17a248');
+
+        $model = $client->publicModels()->faceDetectionModel();
+
+        $input = new ClarifaiFileImage(file_get_contents($image));
+
+        $response = $model->predict($input)->executeSync();
+
+        if ($response->isSuccessful()) {
+            /** @var ClarifaiOutput $output */
+            $output = $response->get();
+            // dd($output->data());
+            if(!empty($output->data())){
+                $humanOnImage = true;
+            }
+        } else {
+            echo "Response is not successful. Reason: \n";
+            echo $response->status()->description() . "\n";
+            echo $response->status()->errorDetails() . "\n";
+            echo "Status code: " . $response->status()->statusCode();
+        }
+
+        return $humanOnImage;
     }
 }
